949f5dbd4fe413e6e1e2120d92b215427726e841
Nicolas Dolinkue <dolinkue_n@hotmail.com>
2022-08-21 10:42:25 -0300

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Nicolas Dolinkue on 21/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
